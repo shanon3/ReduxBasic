f5a9da6a078989dc9b1871902f0acbe37dc2958a
shanon3 <shanonathooya@gmail.com>
2019-10-22 12:11:23 +0700

    Redux

---
+++ src/Redux/AppRedux.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import allReducers from './reducers/index';
+import {createStore} from 'redux';
+import {Provider} from 'react-native';
+import Counter from './Counter';
+
+const store = createStore(allReducers);
+export default class AppRedux extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
+  }
+}
